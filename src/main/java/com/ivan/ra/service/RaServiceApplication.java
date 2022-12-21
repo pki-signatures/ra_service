d6358cf0b25626e5d5c326bf35c82094c83ad265
pki-signatures <pki.signatures.developer@gmail.com>
2022-12-21 19:20:04 +0500

    initial commit

---
+++ src/main/java/com/ivan/ra/service/RaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.ra.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivan/ra/service/RaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.ra.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
